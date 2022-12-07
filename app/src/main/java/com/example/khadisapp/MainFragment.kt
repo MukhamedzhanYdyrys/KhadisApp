269a71c3ea2f07ffe8fe3e53728ae82fedb88a22
MukhamedzhanYdyrys <200103116@stu.sdu.edu.kz>
2022-12-08 04:25:18 +0600

    RecyclerViewAdapter

--- app/src/main/java/com/example/khadisapp/MainFragment.kt
+++ app/src/main/java/com/example/khadisapp/MainFragment.kt
@@ -6,13 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.khadisapp.R
+import com.example.khadisapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    lateinit var binding: FragmentMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
